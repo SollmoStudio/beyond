a98dd4584ac0692c295440468d5b92c63158deff
Kwang Yul Seo <skyul@company100.com>
2014-04-24 01:15:49 +0900

    No need to use a fully qualified name.
    play.api.mvc.AnyContent -> AnyContent.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index : Action[play.api.mvc.AnyContent]= Action {
+  def index : Action[AnyContent]= Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
