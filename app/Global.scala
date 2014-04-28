b4519e2e394490c477a7bf85fe47f3513f588de9
Kwang Yul Seo <skyul@company100.com>
2014-04-28 21:44:38 +0900

    Add a method to Global to return a mongodb connection.
    Override GlobalSettings.onStart to create a MongoConnection and add a method to
    return the instance.
    
    MongoDriver and MongoConnection involve creation costs the driver may create a
    new ActorSystem, and the connection will connect to the servers. It is a good
    idea to store the driver and the connection to reuse them.

--- app/Global.scala
+++ app/Global.scala
@@ -1,12 +1,34 @@
+import play.api.Application
 import play.api.GlobalSettings
 import play.api.Mode
 import play.api.Play
 import play.api.mvc.RequestHeader
 import play.api.mvc.Results.NotFound
 import play.api.mvc.SimpleResult
+import reactivemongo.api.MongoConnection
+import reactivemongo.api.MongoDriver
 import scala.concurrent.Future
 
 object Global extends GlobalSettings {
+  // MongoDriver and MongoConnection involve creation costs
+  // the driver may create a new ActorSystem, and the connection
+  // will connect to the servers. It is a good idea to store the
+  // driver and the connection to reuse them.
+  private var connection: MongoConnection = null
+
+  def mongoConnection: MongoConnection = connection
+
+  override def onStart(app: Application) {
+    // FIXME: MongoDriver creates a new Akka's ActorSystem.
+    // Reuse an existing system to avoid wasting resources.
+    val driver = new MongoDriver
+    connection = driver.connection(List("localhost"))
+  }
+
+  override def onStop(app: Application) {
+    connection = null
+  }
+
   override def onHandlerNotFound(request: RequestHeader): Future[SimpleResult] = {
     Play.maybeApplication.filter(_.mode == Mode.Prod).map { _ =>
       Future.successful(NotFound)
