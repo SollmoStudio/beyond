7a6fb6e9257184b109153e1fdb26999fdf2aeb55
Kwang Yul Seo <skyul@company100.com>
2014-04-29 23:33:17 +0900

    Check password when login.
    Fetch the password of the given user from mongodb and check if the password is
    correct.

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -1,9 +1,13 @@
 package controllers
 
+import beyond.Global
 import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import reactivemongo.api.collections.default.BSONCollection
+import reactivemongo.bson.BSONDocument
+import scala.concurrent.Future
 
 object Admin extends Controller {
   private case class LoginData(username: String, password: String)
@@ -30,16 +34,35 @@ object Admin extends Controller {
     Ok(views.html.admin_login())
   }
 
-  def doLogin : Action[AnyContent] = Action { implicit request =>
+  def doLogin : Action[AnyContent] = Action.async { implicit request =>
     loginForm.bindFromRequest.fold(
       formWithErrors => {
         // FIXME: Create an error type instead of passing String.
-        BadRequest(views.html.admin_login("Invalid username"))
+        val result = BadRequest(views.html.admin_login("Invalid username"))
+        Future.successful(result)
       },
       loginData => {
-        // FIXME: Check password and redirect to login page if the given password is incorrect.
-        val session = "username" -> loginData.username
-        Redirect(routes.Admin.index).withSession(session)
+        import play.api.libs.concurrent.Execution.Implicits._
+
+        val db = Global.mongoConnection.get.db("admin")
+        val collection = db.collection[BSONCollection]("password")
+        val query = BSONDocument("username" -> loginData.username)
+        val filter = BSONDocument("password" -> 1, "_id" -> 0)
+
+        val cursor = collection.find(query, filter).cursor[BSONDocument]
+        // FIXME: How to handle network errors?
+        val result: Future[List[BSONDocument]] = cursor.collect[List]()
+        result.map[SimpleResult] {
+          case List() => BadRequest(views.html.admin_login("No such username"))
+          case passwordDoc :: _ =>
+            val password = passwordDoc.getAs[String]("password").get
+            if (password == loginData.password) {
+              val session = "username" -> loginData.username
+              Redirect(routes.Admin.index).withSession(session)
+            } else {
+              BadRequest(views.html.admin_login("Invalid password"))
+            }
+        }
       }
     )
   }
