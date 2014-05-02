1af0895106b474b5487950a1b24cb9fbbc2b9a89
Kwang Yul Seo <skyul@company100.com>
2014-05-02 17:07:42 +0900

    Extract Header as a RequireJS module.

---
+++ public/javascripts/header.js
@@ -0,0 +1,27 @@
+/** @jsx React.DOM */
+define(function() {
+    return React.createClass({
+        render: function() {
+            var liElements = $.map(this.props.menus, function (value, key) {
+                return <li>
+                    <a href={value}>{key}</a>
+                </li>;
+            });
+
+            return (
+                <div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">{this.props.projectName}</a>
+                        </div>
+                        <div className="navbar-collapse collapse">
+                            <ul className="nav navbar-nav navbar-right">
+                                    {liElements}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                );
+        }
+    });
+});
