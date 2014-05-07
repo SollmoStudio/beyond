16363980866ceeb27adb5085f8df2adeeb469ee4
Kwang Yul Seo <skyul@company100.com>
2014-05-07 16:24:48 +0900

    Use org.mozilla.javascript.tools.shell.Global.
    Use Global as our global scope instead of calling initStandardObjects() by
    ourselves. This is temporary because eventually we want to define our own
    gloabl scope class by extending org.mozilla.javascript.TopLevel.
    
    Also use the loan pattern provided ContextFactory.call(ContextAction). This is
    safer because we don't need to call contextFactory.enterContext() and
    Context.exit() manually.

--- app/beyond/plugin/GamePlugin.scala
+++ app/beyond/plugin/GamePlugin.scala
@@ -1,8 +1,10 @@
 package beyond.plugin
 
 import org.mozilla.javascript.Context
+import org.mozilla.javascript.ContextAction
 import org.mozilla.javascript.Function
-import org.mozilla.javascript.Scriptable
+import org.mozilla.javascript.tools.shell.Global
+import org.mozilla.javascript.tools.shell.QuitAction
 
 trait GamePlugin {
   // FIXME: Pass request parameters along with path and
@@ -14,33 +16,59 @@ trait GamePlugin {
 object GamePlugin {
   private val contextFactory: BeyondContextFactory = new BeyondContextFactory
 
+  // FIXME: Need a new class extending org.mozilla.javascript.TopLevel
+  // because we don't need all powers of Global.
+  private val global: Global = new Global
+
+  // FIXME: Move implicit functions for Rhino interoperability to another package.
+  private implicit def functionToQuitAction(f: (Context, Int) => Unit): QuitAction = {
+    new QuitAction {
+      override def quit(cx: Context, exitCode: Int) {
+        f(cx, exitCode)
+      }
+    }
+  }
+
+  private implicit def functionToContextAction(f: Context => AnyRef): ContextAction = {
+    new ContextAction {
+      override def run(cx: Context): AnyRef = {
+        f(cx)
+      }
+    }
+  }
+
   def apply(): GamePlugin = {
+    // FIXME: We don't want to allow plugins to terminate the system.
+    // Delete this once we replace Global with our own global scope.
+    global.initQuitAction {
+      (_: Context, exitCode: Int) => System.exit(exitCode)
+    }
+
     // FIXME: Don't hardcode plugin source code here.
     val source = "function handle(path) { return 'Hello ' + path; }"
     val cx: Context = contextFactory.enterContext()
     try {
-      val scope: Scriptable = cx.initStandardObjects()
-      cx.evaluateString(scope, source, "source", 1, null)
+      if (!global.isInitialized) {
+        global.init(contextFactory)
+      }
+      cx.evaluateString(global, source, "source", 1, null)
       // FIXME: Don't hardcode the name of handler function.
       // FIXME: handler might be Scriptable.NOT_FOUND if there is no function named "handle".
       // Also, it might not be an instance of Function.
-      val handler = scope.get("handle", scope).asInstanceOf[Function]
-      new GamePluginImpl(scope, handler)
+      val handler = global.get("handle", global).asInstanceOf[Function]
+      new GamePluginImpl(handler)
     } finally {
       Context.exit()
     }
   }
 
-  private class GamePluginImpl(scope: Scriptable, handler: Function) extends GamePlugin {
+  private class GamePluginImpl(handler: Function) extends GamePlugin {
     def handle(path: String): String = {
-      val cx: Context = contextFactory.enterContext()
-      try {
+      val result = contextFactory.call { cx: Context =>
         val args: Array[AnyRef] = Array(path)
-        val result = handler.call(cx, scope, scope, args)
-        result.toString
-      } finally {
-        Context.exit()
+        handler.call(cx, global, global, args)
       }
+      result.toString
     }
   }
 }
