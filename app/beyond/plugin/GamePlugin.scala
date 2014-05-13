7d9e5b881cb5c4a0e7daba258b265d263695fad9
Kwang Yul Seo <skyul@company100.com>
2014-05-13 14:35:02 +0900

    Use ContextFactory.call instead of ContextFactory.enterContext/Context.exit.
    It is easy to forget to call Context.exit in finally. Use the loan pattern
    provided by ContextFactory.call.

--- app/beyond/plugin/GamePlugin.scala
+++ app/beyond/plugin/GamePlugin.scala
@@ -19,33 +19,28 @@ object GamePlugin {
 
   private val global: BeyondGlobal = new BeyondGlobal(contextFactory)
 
-  def apply(filename: String): GamePlugin = {
-    val cx: Context = contextFactory.enterContext()
-    try {
-      import scala.collection.JavaConverters._
-      import play.api.Play.current
+  def apply(filename: String): GamePlugin = contextFactory.call { cx: Context =>
+    import scala.collection.JavaConverters._
+    import play.api.Play.current
 
-      val defaultModulePaths = Seq("plugins")
-      val modulePaths = current.configuration.getStringList("beyond.plugin.path").map(_.asScala).getOrElse(defaultModulePaths)
+    val defaultModulePaths = Seq("plugins")
+    val modulePaths = current.configuration.getStringList("beyond.plugin.path").map(_.asScala).getOrElse(defaultModulePaths)
 
-      // Sandboxed means that the require function doesn't have the "paths"
-      // property, and also that the modules it loads don't export the
-      // "module.uri" property.
-      val sandboxed = true
-      val require = global.installRequire(cx, modulePaths, sandboxed)
-      val exports = require.requireMain(cx, filename)
-      // FIXME: Don't hardcode the name of handler function.
-      // FIXME: handler might be Scriptable.NOT_FOUND if there is no function named "handle".
-      // Also, it might not be an instance of Function.
-      val handler = exports.get("handle", exports).asInstanceOf[Function]
+    // Sandboxed means that the require function doesn't have the "paths"
+    // property, and also that the modules it loads don't export the
+    // "module.uri" property.
+    val sandboxed = true
+    val require = global.installRequire(cx, modulePaths, sandboxed)
+    val exports = require.requireMain(cx, filename)
+    // FIXME: Don't hardcode the name of handler function.
+    // FIXME: handler might be Scriptable.NOT_FOUND if there is no function named "handle".
+    // Also, it might not be an instance of Function.
+    val handler = exports.get("handle", exports).asInstanceOf[Function]
 
-      // FIXME: Pass the module URI once we load scripts from file path.
-      val scope = new ModuleScope(global, null, null)
-      new GamePluginImpl(scope, handler)
-    } finally {
-      Context.exit()
-    }
-  }
+    // FIXME: Pass the module URI once we load scripts from file path.
+    val scope = new ModuleScope(global, null, null)
+    new GamePluginImpl(scope, handler)
+  }.asInstanceOf[GamePlugin]
 
   private class GamePluginImpl(scope: ModuleScope, handler: Function) extends GamePlugin {
     def handle[A](request: Request[A]): String = {
