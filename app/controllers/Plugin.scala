fa0d20eaae71d954b4a9958706075a1d673f7ea6
Kwang Yul Seo <skyul@company100.com>
2014-05-16 01:46:10 +0900

    Change Plugin timeout to beyond.request-timeout.
    If GamePlugin does not complete the future, ask will expire after the timeout
    period, completing it with an AskTimeoutException. 10 seconds here is too
    arbitrary. Use beyond.request-timeout instead.

--- app/controllers/Plugin.scala
+++ app/controllers/Plugin.scala
@@ -4,11 +4,11 @@ import akka.actor.ActorSelection
 import akka.pattern.AskTimeoutException
 import akka.pattern.ask
 import akka.util.Timeout
+import beyond.Global
 import beyond.plugin.GamePlugin.Handle
 import play.api._
 import play.api.libs.concurrent.Akka
 import play.api.mvc._
-import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
@@ -17,8 +17,7 @@ object Plugin extends Controller {
   private val gamePlugin: ActorSelection = Akka.system.actorSelection("/user/beyondSupervisor/gamePlugin")
 
   def route(path: String) : Action[AnyContent] = Action.async { request =>
-    // FIXME: Make timeout configurable.
-    implicit val timeout = Timeout(10 seconds)
+    implicit val timeout = Timeout(Global.requestTimeout)
     implicit val ec: ExecutionContext = Akka.system.dispatcher
 
     (gamePlugin ? Handle(request)).asInstanceOf[Future[String]].map(Ok(_)).recover {
