dcfdfcf9909b6b73a7817db90e2e30915a384308
Seulgi Kim <sgkim126@gmail.com>
2014-05-17 21:48:15 +0900

    Add CuratorFrameworkFactoryWithDefaultPolicy.
    This factory creates a CuratorFramework with the retry policy read from
    configuration.

--- app/beyond/Global.scala
+++ app/beyond/Global.scala
@@ -3,6 +3,8 @@ package beyond
 import akka.actor.ActorRef
 import akka.actor.Props
 import com.typesafe.scalalogging.slf4j.{ StrictLogging => Logging }
+import org.apache.curator.RetryPolicy
+import org.apache.curator.retry.ExponentialBackoffRetry
 import play.api.Application
 import play.api.libs.concurrent.Akka
 import play.api.Mode
@@ -74,6 +76,16 @@ object Global extends WithFilters(TimeoutFilter) with Logging {
     import scala.collection.JavaConverters._
     val defaultModulePaths = Seq("plugins")
     current.configuration.getStringList("beyond.plugin.path").map(_.asScala).getOrElse(defaultModulePaths)
+  }
+
+  def curatorConnectionPolicy: RetryPolicy = {
+    val configuration = play.api.Play.current.configuration
+    val defaultMaxRetries = 10
+    val curatorPath = "beyond.curator.connection."
+    val baseSleepTimeMs = Duration(configuration.getString(curatorPath + "base-sleep-time").getOrElse("1000ms")).toMillis.toInt
+    val maxRetries = configuration.getInt(curatorPath + "max-retries").getOrElse(defaultMaxRetries)
+    val maxSleepMs = Duration(configuration.getString(curatorPath + "max-sleep").getOrElse("1000ms")).toMillis.toInt
+    new ExponentialBackoffRetry(baseSleepTimeMs, maxRetries, maxSleepMs)
   }
 }
 
