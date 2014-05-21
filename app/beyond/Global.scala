4e57cfd7819bb5ef01d30afe24c9de8cd5025afc
Seulgi Kim <seulgikim@company100.com>
2014-05-21 14:09:28 +0900

    Override configuration with mode specific cofiguration.

--- app/beyond/Global.scala
+++ app/beyond/Global.scala
@@ -2,14 +2,17 @@ package beyond
 
 import akka.actor.ActorRef
 import akka.actor.Props
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.slf4j.{ StrictLogging => Logging }
+import java.io.File
 import java.net.InetAddress
 import org.apache.curator.RetryPolicy
 import org.apache.curator.retry.ExponentialBackoffRetry
 import play.api.Application
-import play.api.libs.concurrent.Akka
+import play.api.Configuration
 import play.api.Mode
 import play.api.Play
+import play.api.libs.concurrent.Akka
 import play.api.libs.concurrent.Promise
 import play.api.mvc.Filter
 import play.api.mvc.RequestHeader
@@ -37,6 +40,22 @@ private object TimeoutFilter extends Filter {
 
 object Global extends WithFilters(TimeoutFilter) with Logging {
   private var beyondSupervisor: Option[ActorRef] = _
+
+  override def onLoadConfig(defaultConfig: Configuration, path: File, classLoader: ClassLoader, mode: Mode.Mode): Configuration = {
+    def loadConfigFromFile(configName: String): Configuration = {
+      val file = new File(path, s"conf/$configName")
+      if (file.exists) {
+        logger.info(s"Load ${file.getCanonicalPath}")
+        Configuration(ConfigFactory.parseFile(file))
+      } else {
+        Configuration.empty
+      }
+    }
+    val modeSpecificConfiguration = loadConfigFromFile(s"application.${mode.toString.toLowerCase}.conf")
+
+    val finalConfiguration = defaultConfig ++ modeSpecificConfiguration
+    super.onLoadConfig(finalConfiguration, path, classLoader, mode)
+  }
 
   override def onStart(app: Application) {
     logger.info("Beyond started")
