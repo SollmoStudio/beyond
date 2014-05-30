e4655d9a2bf5e6d49b592cf607d9ad962a52d9c2
Seulgi Kim <sgkim126@gmail.com>
2014-05-30 17:25:36 +0900

    Make UserActionActor request routing table to UserActionActor.
    Currently, once UserActionActor is restarted UserActionActor has a
    invalid routing table until RoutingTableWorker notifies.
    
    This patch makes UserActionActor request routing table to
    UserActionSupervisor when it restarts.

--- app/beyond/UserActionActor.scala
+++ app/beyond/UserActionActor.scala
@@ -4,6 +4,7 @@ import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.pattern.pipe
 import akka.routing.ConsistentHashingRouter.ConsistentHashable
+import beyond.UserActionSupervisor.RequestRoutingTable
 import beyond.route.Address
 import beyond.route.RoutingTableView
 import beyond.route.RoutingTableView._
@@ -31,6 +32,12 @@ object UserActionActor {
 
 private class UserActionActor extends Actor with ActorLogging {
   var routingTable: RoutingTableView = new RoutingTableView(BeyondConfiguration.currentServerAddress)
+  private val userActionSupervisor = {
+    import play.api.libs.concurrent.Akka
+    import play.api.Play.current
+    implicit val ec: ExecutionContext = Akka.system.dispatcher
+    Akka.system.actorSelection(BeyondSupervisor.UserActionSupervisorPath)
+  }
   import UserActionActor._
   override def receive: Receive = {
     case BlockAndRequest(block, request) =>
@@ -49,5 +56,9 @@ private class UserActionActor extends Actor with ActorLogging {
     case UpdateRoutingTable(jsRoutingTable) =>
       log.info("Routing table is updated.")
       routingTable = new RoutingTableView(BeyondConfiguration.currentServerAddress, jsRoutingTable)
+  }
+
+  override def preStart() {
+    userActionSupervisor ! RequestRoutingTable
   }
 }
