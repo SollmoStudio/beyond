acb33531649eaccee4ad725083afee846576bc53
Seulgi Kim <seulgikim@company100.com>
2014-06-05 17:41:15 +0900

    Extract the method to get a process ID.

--- app/beyond/BeyondRuntime.scala
+++ app/beyond/BeyondRuntime.scala
@@ -4,6 +4,8 @@ import java.io.File
 import java.net.URLClassLoader
 import play.api.Mode
 import scalax.file.Path
+import java.lang.management.ManagementFactory
+import java.lang.management.RuntimeMXBean
 
 object BeyondRuntime {
   lazy val classPath: String = {
@@ -21,4 +23,15 @@ object BeyondRuntime {
 
   val javaPath: String =
     (Path.fromString(System.getProperty("java.home")) / "bin" / "java").path
+
+  val processID: String = {
+    val bean: RuntimeMXBean = ManagementFactory.getRuntimeMXBean
+    // Get the name representing the running Java virtual machine.
+    // It returns something like 6460@AURORA. Where the value
+    // before the @ symbol is the PID.
+    val jvmName = bean.getName
+    // Extract the PID by splitting the string returned by the
+    // bean.getName() method.
+    jvmName.split("@")(0)
+  }
 }
