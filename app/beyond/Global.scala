c3b4d80dfabad8c896674353ec28348d1ae56a6b
Seulgi Kim <seulgikim@company100.com>
2014-06-12 13:38:34 +0900

    Add RequestsCountFilter.
    RequestsCountFilter is a global filter. It should be on the first in
    WithFilters to count all requests, because filters are applied in the
    order.

--- app/beyond/Global.scala
+++ app/beyond/Global.scala
@@ -2,6 +2,7 @@ package beyond
 
 import akka.actor.ActorRef
 import akka.actor.Props
+import beyond.metrics.RequestsCountFilter
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.slf4j.{ StrictLogging => Logging }
 import java.io.File
@@ -16,7 +17,7 @@ import play.api.mvc.SimpleResult
 import play.api.mvc.WithFilters
 import scala.concurrent.Future
 
-object Global extends WithFilters(TimeoutFilter) with Logging {
+object Global extends WithFilters(RequestsCountFilter, TimeoutFilter) with Logging {
   private var beyondSupervisor: Option[ActorRef] = _
 
   override def onLoadConfig(defaultConfig: Configuration, path: File, classLoader: ClassLoader, mode: Mode.Mode): Configuration = {
