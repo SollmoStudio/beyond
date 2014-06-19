cdd306c0b3ce6e62898d17e69ed8f157eebc336a
Seulgi Kim <seulgikim@company100.com>
2014-06-19 18:26:12 +0900

    Add util.format API.
    It has the same placeholder with the util.format of node.js.
    The callback function of replace is copied from node.js.
    See https://github.com/joyent/node/blob/master/lib/util.js
    
    And this patch also adds plugins/lib to plugin path.

---
+++ plugins/lib/util.js
@@ -0,0 +1,35 @@
+exports.format = function (inputString) {
+    var extraTokens,
+        index,
+        middleStage,
+        pattern,
+        tokens;
+    tokens = Array.prototype.slice.call(arguments, 1); // arguments[0] is inputString.
+    index = 0;
+    pattern = /%[sdj%]/g;
+    middleStage = String(inputString).replace(pattern, function (placeholder) {
+        var ex;
+        if (index >= tokens.length) {
+            return placeholder;
+        }
+        switch (placeholder) {
+            case '%s':
+                return String(tokens[index++]);
+            case '%d':
+                return Number(tokens[index++]);
+            case '%j':
+                try {
+                    return JSON.stringify(tokens[index++]);
+                } catch (ex) {
+                    return '...';
+                }
+            case '%%':
+                return '%';
+            default:
+                return placeholder;
+        }
+    });
+    extraTokens = tokens.slice(index);
+    extraTokens.unshift(middleStage);
+    return extraTokens.join(' ');
+}
