7cc20b9f7f13306510920f158cc34d475e08a489
Seulgi Kim <seulgikim@company100.com>
2014-06-20 20:06:39 +0900

    Add event.track API.
    This API is for logging game event with JavaScript.
    The first argument is tag, and others are messages.
    
    The messages are logged in the one of the four form.
     - Object -> message itself.
     - Only one message -> { "message": message }
     - Multiple messages -> { "message": [ message1, message2, ... ] }
     - No messages or message are null or undefined -> {} (empty object)

---
+++ plugins/lib/event.js
@@ -0,0 +1,20 @@
+var util = require('util');
+importClass(Packages.beyond.plugin.lib.PluginEvent);
+var nativeEvent = new PluginEvent();
+exports.track = function (tag) {
+    var formatString,
+        i,
+        tokenLength,
+        tokens;
+    tokens = Array.prototype.slice.call(arguments, 1); // arguments[0] is tag.
+    tokenLength = tokens.length;
+    if (tokenLength !== 0) {
+        formatString = tokens.map(function () { return '%j'; }).join(', ')
+        if (tokenLength > 1) {
+            formatString = '[' + formatString + ']';
+        }
+        tokens.unshift(formatString);
+    }
+    message = util.format.apply(util.format, tokens);
+    nativeEvent.track(tag, message);
+}
