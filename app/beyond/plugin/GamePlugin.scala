24d6d2dba8daf3e1efc29c0f0c051a2b1e592432
Seulgi Kim <seulgikim@company100.com>
2014-06-25 10:30:47 +0900

    Implement timer for GamePlugin.

--- app/beyond/plugin/GamePlugin.scala
+++ app/beyond/plugin/GamePlugin.scala
@@ -2,11 +2,18 @@ package beyond.plugin
 
 import akka.actor.Actor
 import akka.actor.ActorLogging
+import akka.actor.Cancellable
 import akka.actor.Props
 import akka.routing.RoundRobinRouter
+import beyond.plugin.GamePlugin.InvokeFunction
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Function
+import org.mozilla.javascript.Scriptable
 import play.api.mvc.Request
+import scala.concurrent.duration._
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
 
 object GamePlugin {
   val Name: String = "gamePlugin"
@@ -18,7 +25,7 @@ object GamePlugin {
 class NoHandlerFunctionFoundException extends Exception
 
 // FIXME: Handle script errors.
-class GamePlugin(filename: String) extends Actor with ActorLogging {
+class GamePlugin(filename: String) extends Actor with ActorLogging with JavaScriptTimerProvider {
   import com.beyondframework.rhino.RhinoConversions._
 
   private val engine = new BeyondJavaScriptEngine
@@ -46,5 +53,55 @@ class GamePlugin(filename: String) extends Actor with ActorLogging {
   override def receive: Receive = {
     case msg => workerActor forward msg
   }
+
+  override def setTimeout(thisObj: Scriptable, args: Array[AnyRef], funObj: Function): Try[AnyRef] =
+    if (args.length < 2) {
+      Failure(new IllegalArgumentException("args.are.not.enough"))
+    } else if (!args(0).isInstanceOf[Function]) {
+      Failure(new IllegalArgumentException("first.arg.is.not.function"))
+    } else {
+      val callback = args(0).asInstanceOf[Function]
+      val callbackArgs = args.drop(2)
+      val delay = Context.toNumber(args(1)).millis
+
+      import context.dispatcher
+      Success(context.system.scheduler.scheduleOnce(delay, self, InvokeFunction(callback, callbackArgs)))
+    }
+
+  override def setInterval(thisObj: Scriptable, args: Array[AnyRef], funObj: Function): Try[AnyRef] =
+    if (args.length < 2) {
+      Failure(new IllegalArgumentException("args.are.not.enough"))
+    } else if (!args(0).isInstanceOf[Function]) {
+      Failure(new IllegalArgumentException("first.arg.is.not.function"))
+    } else {
+      val callback = args(0).asInstanceOf[Function]
+      val callbackArgs = args.drop(2)
+      val delay = Context.toNumber(args(1)).millis
+
+      import context.dispatcher
+      Success(context.system.scheduler.schedule(initialDelay = delay, interval = delay, self, InvokeFunction(callback, callbackArgs)))
+    }
+
+  override def clearTimeout(thisObj: Scriptable, args: Array[AnyRef], funObj: Function): Try[Unit] =
+    if (args.length == 0) {
+      Failure(new IllegalArgumentException("args.length.is.zero"))
+    } else if (!args(0).isInstanceOf[Cancellable]) {
+      Failure(new IllegalArgumentException("first.arg.is.not.timeout.object"))
+    } else {
+      val id = args(0).asInstanceOf[Cancellable]
+      id.cancel()
+      Success(Unit)
+    }
+
+  override def clearInterval(thisObj: Scriptable, args: Array[AnyRef], funObj: Function): Try[Unit] =
+    if (args.length == 0) {
+      Failure(new IllegalArgumentException("args.length.is.zero"))
+    } else if (!args(0).isInstanceOf[Cancellable]) {
+      Failure(new IllegalArgumentException("first.arg.is.not.timeout.object"))
+    } else {
+      val id = args(0).asInstanceOf[Cancellable]
+      id.cancel()
+      Success(Unit)
+    }
 }
 
