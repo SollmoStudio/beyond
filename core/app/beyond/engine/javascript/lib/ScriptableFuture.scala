a9d7fa2af8eb6ff1268dbc27a46e923f205f9ef7
Seulgi Kim <dev@seulgik.im>
2014-06-30 01:17:07 +0900

    Introduce future API on JavaScript.
    There are three function to create Future.
     - create
        Create new future with the given callback.
        The callback will be execued in the execution context of the engine.
     - successful
        Create future with the given value.
     - failed
        Create future with the given error message.
    
    This patch adds onComplete, onSuccess and onFailure method to Future
    object. These three method take one callback function, but the argument
    type of the callback function is different.
    And these three methods return itself to help method chaining.
    
    See https://github.com/SollmoStudio/beyond/wiki/Beyond-Future-API for
    more detail.

---
+++ core/app/beyond/engine/javascript/lib/ScriptableFuture.scala
@@ -0,0 +1,81 @@
+package beyond.engine.javascript.lib
+
+import beyond.engine.javascript.BeyondContext
+import beyond.engine.javascript.BeyondContextFactory
+import java.lang.{ Boolean => JavaBoolean }
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.ContextFactory
+import org.mozilla.javascript.Function
+import org.mozilla.javascript.Scriptable
+import org.mozilla.javascript.ScriptableObject
+import scala.concurrent.Future
+import scala.util.Failure
+import scala.util.Success
+
+object ScriptableFuture {
+  import com.beyondframework.rhino.RhinoConversions._
+
+  private def executeCallback(contextFactory: ContextFactory, callback: Function, callbackArgs: Array[AnyRef]) = {
+    val beyondContextFactory = contextFactory.asInstanceOf[BeyondContextFactory]
+    val scope = beyondContextFactory.global
+    beyondContextFactory.call { context: Context =>
+      callback.call(context, scope, scope, callbackArgs)
+    }
+  }
+
+  def jsFunction_onComplete(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function): ScriptableFuture = {
+    implicit val executionContext = context.asInstanceOf[BeyondContext].executionContext
+    val callback = args(0).asInstanceOf[Function]
+    val thisFuture = thisObj.asInstanceOf[ScriptableFuture]
+    thisFuture.future.onComplete { futureResult =>
+      val callbackArgs: Array[AnyRef] = futureResult match {
+        case Success(result) =>
+          Array(result, new JavaBoolean(true))
+        case Failure(throwable) =>
+          Array(throwable.getMessage, new JavaBoolean(false))
+      }
+      executeCallback(context.getFactory, callback, callbackArgs)
+    }
+    thisFuture
+  }
+
+  def jsFunction_onSuccess(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function): ScriptableFuture = {
+    implicit val executionContext = context.asInstanceOf[BeyondContext].executionContext
+    val callback = args(0).asInstanceOf[Function]
+    val thisFuture = thisObj.asInstanceOf[ScriptableFuture]
+    thisFuture.future.onSuccess {
+      case result: AnyRef =>
+        val callbackArgs: Array[AnyRef] = Array(result)
+        executeCallback(context.getFactory, callback, callbackArgs)
+    }
+    thisFuture
+  }
+
+  def jsFunction_onFailure(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function): ScriptableFuture = {
+    implicit val executionContext = context.asInstanceOf[BeyondContext].executionContext
+    val callback = args(0).asInstanceOf[Function]
+    val thisFuture = thisObj.asInstanceOf[ScriptableFuture]
+    thisFuture.future.onFailure {
+      case throwable: Throwable =>
+        val callbackArgs: Array[AnyRef] = Array(throwable.getMessage)
+        executeCallback(context.getFactory, callback, callbackArgs)
+    }
+    thisFuture
+  }
+
+  def jsConstructor(context: Context, args: Array[AnyRef], constructor: Function, inNewExpr: Boolean): ScriptableFuture = {
+    implicit val executionContext = context.asInstanceOf[BeyondContext].executionContext
+    val scalaFuture = Future {
+      val callback = args(0).asInstanceOf[Function]
+      val callbackArgs = Array.empty[AnyRef]
+      executeCallback(context.getFactory, callback, callbackArgs)
+    }
+    new ScriptableFuture(scalaFuture)
+  }
+}
+
+class ScriptableFuture(val future: Future[AnyRef]) extends ScriptableObject {
+  def this() = this(null)
+
+  override def getClassName: String = "Future"
+}
