f53fd46538b4712b85423ddd8d6585fee51596a0
Seulgi Kim <dev@seulgik.im>
2014-06-30 01:16:38 +0900

    Share GlobalScope among GamePluginWorkers.
    Currently, each GamePluginWorker has their own ModuleScope. It makes
    recovery scope easy because restarting the worker that has corrupted
    scope recovers the scope.
    
    But we implemented timer APIs(setTimer and setInterval) and will
    implement Future API. These API don't ensure the callback is executed in
    the same worker. So we decided share the scope among GamePluginWorkers.

--- core/app/beyond/plugin/GamePluginWorker.scala
+++ core/app/beyond/plugin/GamePluginWorker.scala
@@ -5,17 +5,14 @@ import beyond.engine.javascript.BeyondJavaScriptEngine
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Function
 import org.mozilla.javascript.Scriptable
-import org.mozilla.javascript.commonjs.module.ModuleScope
 import play.api.mvc.Request
 
 class GamePluginWorker(engine: BeyondJavaScriptEngine, handler: Function) extends Actor {
   import com.beyondframework.rhino.RhinoConversions._
   import beyond.plugin.GamePlugin._
 
-  // FIXME: Pass the module URI once we load scripts from file path.
-  val scope = new ModuleScope(engine.global, null, null)
-
   private def handle[A](request: Request[A]): String = engine.contextFactory.call { cx: Context =>
+    val scope = engine.global
     val scriptableRequest: Scriptable = cx.newObject(scope, "Request", Array(request))
     val args: Array[AnyRef] = Array(scriptableRequest)
     handler.call(cx, scope, scope, args)
@@ -23,6 +20,7 @@ class GamePluginWorker(engine: BeyondJavaScriptEngine, handler: Function) extend
 
   private def invokeFunction(function: Function, args: Array[AnyRef]) {
     engine.contextFactory.call { cx: Context =>
+      val scope = engine.global
       function.call(cx, scope, scope, args)
     }
   }
