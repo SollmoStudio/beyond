3b240928c959c32b6c871f2423ff5fcf267eb203
Seulgi Kim <dev@seulgik.im>
2014-06-30 11:37:18 +0900

    Add ScriptableConsole.
    The next patch will define ScriptableConsole in the BeyondGlobal.

---
+++ core/app/beyond/engine/javascript/lib/ScriptableConsole.scala
@@ -0,0 +1,36 @@
+package beyond.engine.javascript.lib
+
+import beyond.engine.javascript.BeyondContext
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Function
+import org.mozilla.javascript.Scriptable
+import org.mozilla.javascript.ScriptableObject
+
+object ScriptableConsole {
+  def jsFunction_log(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function) {
+    context.asInstanceOf[BeyondContext].console.log(args(0).asInstanceOf[String])
+  }
+
+  def jsFunction_info(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function) {
+    context.asInstanceOf[BeyondContext].console.info(args(0).asInstanceOf[String])
+  }
+
+  def jsFunction_warn(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function) {
+    context.asInstanceOf[BeyondContext].console.warn(args(0).asInstanceOf[String])
+  }
+
+  def jsFunction_debug(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function) {
+    context.asInstanceOf[BeyondContext].console.debug(args(0).asInstanceOf[String])
+  }
+
+  def jsFunction_error(context: Context, thisObj: Scriptable, args: Array[AnyRef], function: Function) {
+    context.asInstanceOf[BeyondContext].console.error(args(0).asInstanceOf[String])
+  }
+
+  def jsConstructor(context: Context, args: Array[AnyRef], constructor: Function, inNewExpr: Boolean): ScriptableConsole =
+    new ScriptableConsole
+}
+
+class ScriptableConsole extends ScriptableObject {
+  override def getClassName: String = "Console"
+}
