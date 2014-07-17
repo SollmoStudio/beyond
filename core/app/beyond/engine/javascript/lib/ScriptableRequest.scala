61ad3fe84df2d491b8206916b6b89d761b437761
Kwang Yul Seo <skyul@company100.com>
2014-07-17 10:18:46 +0900

    Use the Scala constructor annotation syntax.

--- core/app/beyond/engine/javascript/lib/ScriptableRequest.scala
+++ core/app/beyond/engine/javascript/lib/ScriptableRequest.scala
@@ -1,14 +1,14 @@
 package beyond.engine.javascript.lib
 
 import org.mozilla.javascript.ScriptableObject
-import org.mozilla.javascript.annotations.{JSConstructor, JSGetter}
+import org.mozilla.javascript.annotations.JSConstructor
+import org.mozilla.javascript.annotations.JSGetter
 import play.api.mvc.Request
 
 // FIXME: Add more members.
 // The type of param must be AnyRef.
 // Otherwise, Rhino throws an exception when checking parameter types.
-@JSConstructor
-class ScriptableRequest[A](param: AnyRef) extends ScriptableObject {
+class ScriptableRequest[A] @JSConstructor()(param: AnyRef) extends ScriptableObject {
   def this() = this(null)
 
   private val request: Request[A] = param.asInstanceOf[Request[A]]
