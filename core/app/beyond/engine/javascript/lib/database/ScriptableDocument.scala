1374d4dd6823acf3c89f67ac9d0d83d6c251eee3
Seulgi Kim <dev@seulgik.im>
2014-07-24 23:25:08 +0900

    Implement ScriptableDocument constructor.
    This constructor is used internally. Users are not allowed to construct
    an instance directly. A user must get a ScriptableDocument from
    ScriptableCollection.

--- core/app/beyond/engine/javascript/lib/database/ScriptableDocument.scala
+++ core/app/beyond/engine/javascript/lib/database/ScriptableDocument.scala
@@ -3,12 +3,20 @@ package beyond.engine.javascript.lib.database
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Function
 import org.mozilla.javascript.ScriptableObject
+import reactivemongo.bson.BSONDocument
 
 object ScriptableDocument {
-  def jsConstructor(context: Context, args: Array[AnyRef], constructor: Function, inNewExpr: Boolean): ScriptableDocument =
-    new ScriptableDocument
+  // This constructor is used internally. Users are not allowed to construct an instance directly.
+  // A user must get a ScriptableDocument from either ScriptableCollection.find() or ScriptableCollection.findOne().
+  def jsConstructor(context: Context, args: Array[AnyRef], constructor: Function, inNewExpr: Boolean): ScriptableDocument = {
+    val fields = args(0).asInstanceOf[Seq[Field]]
+    val currentValueInDB = args(1).asInstanceOf[BSONDocument]
+    new ScriptableDocument(fields, currentValueInDB)
+  }
 }
 
-class ScriptableDocument extends ScriptableObject {
+class ScriptableDocument(fields: Seq[Field], currentValueInDB: BSONDocument) extends ScriptableObject {
+  def this() = this(Seq.empty, BSONDocument.empty)
+
   override val getClassName: String = "Document"
 }
