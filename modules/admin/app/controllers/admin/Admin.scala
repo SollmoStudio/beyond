65ac25fc23681f7ae086f4ee7cf1072c7f0f21b7
Hyeonje Alex Jun <noraesae@company100.com>
2014-07-25 00:59:11 +0100

    Scalastyle linting in admin submodule.
    Public method must have explicit type.

--- modules/admin/app/controllers/admin/Admin.scala
+++ modules/admin/app/controllers/admin/Admin.scala
@@ -21,7 +21,7 @@ object Admin extends Controller with MongoController {
 
   private class AuthenticatedRequest[A](val username: String, request: Request[A]) extends WrappedRequest[A](request)
   private object AuthenticatedAction extends ActionBuilder[AuthenticatedRequest] {
-    def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]) = {
+    def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]): Future[Result] = {
       request.session.get("username").map { username =>
         block(new AuthenticatedRequest(username, request))
       } getOrElse {
