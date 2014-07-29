db194af07c642e0144139fb965f4ab20da8869ed
Seulgi Kim <dev@seulgik.im>
2014-07-29 19:52:45 +0900

    Add some DB API(insert, find and findOne) examples.
    This patch adds examples to show how to use DB API.
    
    There are three examples:
     - insert
     - find
     - findOne
    
    To run insert example, send POST to /plugins/insert/{key}/{value}.
    This example inserts the key and value to example.keyValue collection.
    
    To run find example, send POST to /plugins/find/{key1}[/{keyN}]
    This example finds all the documents whose key is one of keyN.
    
    To run findOne example, send POST to /plugins/findOne/{key1}[/{keyN}]
    This example finds the document whose key is one of keyN.

--- plugins/main.js
+++ plugins/main.js
@@ -1,6 +1,25 @@
 var counter = require("counter");
+var db = require("examples/db");
 
-exports.handle = function(req) {
-    return 'Hello ' + req.uri + ' ' + counter.count();
-};
-
+exports.handle = function (req) {
+    var tokens = req.uri.split("/");
+    tokens.shift();
+    tokens.shift();
+    var operator = tokens.shift();
+    switch (operator) {
+        case "counter":
+            return 'Hello ' + req.uri + ' ' + counter.count();
+        case "insert":
+            db.insert(tokens[0], tokens[1]);
+            break;
+        case "find":
+            db.find.apply(db.find, tokens);
+            break;
+        case "findOne":
+            db.findOne.apply(db.findOne, tokens);
+            break;
+        default:
+            break;
+    }
+    return "";
+}
