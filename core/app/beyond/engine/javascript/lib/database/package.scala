49ff75287677931e3b2c27867bef35e540039dc4
Seulgi Kim <dev@seulgik.im>
2014-07-31 19:03:50 +0900

    Convert to requested type when read document from database.
    Currently, ScirptableDocument reads data only if the saved data's type
    is exactly same as the specified type in schema.
    This patch makes ScriptableDocument convert the data with Rhino engine
    if available.

--- core/app/beyond/engine/javascript/lib/database/package.scala
+++ core/app/beyond/engine/javascript/lib/database/package.scala
@@ -7,15 +7,18 @@ import org.mozilla.javascript.ScriptRuntime
 import reactivemongo.bson.BSONBoolean
 import reactivemongo.bson.BSONDateTime
 import reactivemongo.bson.BSONDouble
+import reactivemongo.bson.BSONHandler
 import reactivemongo.bson.BSONInteger
 import reactivemongo.bson.BSONJavaScript
 import reactivemongo.bson.BSONLong
+import reactivemongo.bson.BSONReader
 import reactivemongo.bson.BSONString
 import reactivemongo.bson.BSONValue
 import reactivemongo.bson.BSONWriter
 
 package object database {
-  implicit object AnyRefBSONWriter extends BSONWriter[AnyRef, BSONValue] {
+
+  private[database] implicit object AnyRefBSONHandler extends BSONHandler[BSONValue, AnyRef] {
     override def write(value: AnyRef): BSONValue = value match {
       case s: String => BSONString(s)
       case i: jl.Integer => BSONInteger(i)
@@ -26,6 +29,17 @@ package object database {
       case f: Function => BSONJavaScript(f.toString)
       case _ =>
         throw new IllegalArgumentException(s"$value(${value.getClass} cannot be a BSONValue")
+    }
+
+    override def read(bson: BSONValue): AnyRef = bson match {
+      case BSONString(s) => s
+      case BSONInteger(i) => Int.box(i)
+      case BSONLong(j) => Long.box(j)
+      case BSONBoolean(z) => Boolean.box(z)
+      case BSONDouble(d) => Double.box(d)
+      case BSONDateTime(d) => new Date(d)
+      case _ =>
+        throw new IllegalArgumentException(s"$bson cannot be converted")
     }
   }
 
@@ -52,7 +66,7 @@ package object database {
   private[database] case class DoubleField(override val name: String) extends Field
   private[database] case class LongField(override val name: String) extends Field
 
-  object AnyRefTypedBSONWriter {
+  private[database] object AnyRefTypedBSONHandler {
     private def convertToFieldValue(field: Field, value: AnyRef): AnyRef = field match {
       case _: BooleanField =>
         Boolean.box(ScriptRuntime.toBoolean(value))
@@ -73,9 +87,14 @@ package object database {
         Long.box(ScriptRuntime.toInteger(value).toLong)
     }
 
-    def write(field: Field, value: AnyRef): BSONValue = {
+    def write(field: Field, value: AnyRef)(implicit writer: BSONWriter[AnyRef, BSONValue]): BSONValue = {
       val fieldValue: AnyRef = convertToFieldValue(field, value)
-      AnyRefBSONWriter.write(fieldValue)
+      AnyRefBSONHandler.write(fieldValue)
+    }
+
+    def read(field: Field, bson: BSONValue)(implicit reader: BSONReader[BSONValue, AnyRef]): AnyRef = {
+      val valueFromBSON: AnyRef = reader.read(bson)
+      convertToFieldValue(field, valueFromBSON)
     }
   }
 }
