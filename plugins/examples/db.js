2ea3788897c9086f27b361d26ab0cac93b8e8577
Seulgi Kim <dev@seulgik.im>
2014-07-31 17:55:02 +0900

    Support Date type on DB API.
    Currently, there is a bug so Date type is not saved.
    This patch fixes the bug and add example of saving Date.

--- plugins/examples/db.js
+++ plugins/examples/db.js
@@ -2,11 +2,15 @@ var console = require('console')
 var db = require('db')
 var util = require('util')
 
-var schema = new db.Schema(1, { key: { type: 'string'}, value: {type: 'double'}})
+var schema = new db.Schema(1, {
+    key: { type: 'string' },
+    value: { type: 'double' },
+    time: { type: 'date' }
+})
 var collection = new Collection("example.keyValue", schema);
 
 exports.insert = function (key, value) {
-    return collection.insert({key: key, value: value}).onComplete(console.log);
+    return collection.insert({key: key, value: value, time: new Date()}).onComplete(console.log);
 }
 
 exports.find = function () {
@@ -20,7 +24,7 @@ exports.find = function () {
         if (isSuccess) {
             console.log(util.format("Find %d keyValue", result.length));
             result.map(function (keyValue) {
-                console.log(util.format("key: %s value: %s", keyValue.key(), keyValue.value()));
+                console.log(util.format("key: %s value: %s time: %s", keyValue.key(), keyValue.value()), keyValue.time());
             });
         } else {
             console.error(util.format("Cannot find data. ERROR: %s", result));
@@ -37,7 +41,7 @@ exports.findOne = function () {
     var q = baseQuery.or.apply(baseQuery, queries);
     return collection.findOne(q).onComplete(function (result, isSuccess) {
         if (isSuccess) {
-            console.log(util.format("key: %s value: %s", result.key(), result.value()));
+            console.log(util.format("key: %s value: %s time: %s", result.key(), result.value()), result.time());
         } else {
             console.error(util.format("Cannot find data. ERROR: %s", result));
         }
