d71cb6b4622f9326d9a27488e05cc9b36554272c
Hyeonje Alex Jun <noraesae@company100.com>
2014-08-12 02:10:22 +0100

    Implement ScriptableFileSystem.
    It's an interface to provide JavaScript API for handling file system.
    Now only basic functions are implemented to read and write files.
    The API design is found in the Wiki page below.
    
    https://github.com/SollmoStudio/beyond/wiki/Beyond-File-System-API

---
+++ core/app/beyond/engine/javascript/lib/filesystem/ScriptableFileSystem.scala
@@ -0,0 +1,82 @@
+package beyond.engine.javascript.lib.filesystem
+
+import beyond.engine.javascript.BeyondContext
+import beyond.engine.javascript.JSArray
+import beyond.engine.javascript.JSFunction
+import beyond.engine.javascript.lib.ScriptableFuture
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Scriptable
+import org.mozilla.javascript.ScriptableObject
+import org.mozilla.javascript.annotations.{ JSStaticFunction => JSStaticFunctionAnnotation }
+import play.api.Configuration
+import scala.concurrent.ExecutionContext
+import scala.concurrent.future
+import scala.sys.process._
+import scalax.io.Codec
+import scalax.io.Resource
+
+object ScriptableFileSystem {
+  private def parseOptions(args: JSArray, idx: Int): Configuration = {
+    if (!args.isDefinedAt(idx)) {
+      Configuration.empty
+    } else {
+      val options = args(idx).asInstanceOf[ScriptableObject]
+      Configuration.from(options.getIds.map { id =>
+        val key = id.asInstanceOf[String]
+        (key, options.get(key, options))
+      } toMap)
+    }
+  }
+
+  private def setMode(fileName: String, mode: Int) {
+    // FIXME: Doesn't work for Windows
+    val modeStr = mode.toOctalString
+    s"chmod $modeStr $fileName" !!
+  }
+
+  val DefaultEncoding = "UTF-8"
+  val DefaultMode = 438 // aka 0666
+
+  @JSStaticFunctionAnnotation
+  def readFile(context: Context, thisObj: Scriptable, args: JSArray, function: JSFunction): ScriptableFuture = {
+    implicit val executionContext = context.asInstanceOf[BeyondContext].executionContext
+
+    val fileName = args(0).asInstanceOf[String]
+    val options = parseOptions(args, 1)
+
+    implicit val encoding = options.getString("encoding").getOrElse(DefaultEncoding)
+
+    val readFuture = future {
+      val in = Resource.fromFile(fileName)
+      in.string
+    }
+    ScriptableFuture(context, readFuture)
+  }
+
+  @JSStaticFunctionAnnotation
+  def writeFile(context: Context, thisObj: Scriptable, args: JSArray, function: JSFunction): ScriptableFuture = {
+    implicit val executionContext = context.asInstanceOf[BeyondContext].executionContext
+
+    val fileName = args(0).asInstanceOf[String]
+    val data = args(1).asInstanceOf[String]
+    val options = parseOptions(args, 2)
+
+    implicit val encoding = options.getString("encoding").getOrElse(DefaultEncoding)
+    val mode = options.getInt("mode").getOrElse(DefaultMode)
+
+    val writeFuture = future {
+      // FIXME: Loading a file and setting a mode to file should be atomic.
+      val out = Resource.fromFile(fileName)
+      out.write(data)
+      setMode(fileName, mode)
+    }
+    ScriptableFuture(context, writeFuture)
+  }
+
+  def jsConstructor(context: Context, args: JSArray, constructor: JSFunction, inNewExpr: Boolean): ScriptableFileSystem =
+    new ScriptableFileSystem
+}
+
+class ScriptableFileSystem extends ScriptableObject {
+  override def getClassName: String = "FileSystem"
+}
