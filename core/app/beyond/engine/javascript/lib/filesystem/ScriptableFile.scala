52fe4dc8df71bd56d051c4bdb9e2994f4f9fc38c
Hyeonje Alex Jun <noraesae@company100.com>
2014-08-19 23:06:39 +0100

    Add ScriptableFile.
    ScriptableFile is a wrapper of Java File class for Rhino. Currently only
    `name`, `path`, `isFile` and `isDirectory` getters are implemented.

---
+++ core/app/beyond/engine/javascript/lib/filesystem/ScriptableFile.scala
@@ -0,0 +1,48 @@
+package beyond.engine.javascript.lib.filesystem
+
+import beyond.engine.javascript.BeyondContextFactory
+import beyond.engine.javascript.JSArray
+import beyond.engine.javascript.JSFunction
+import java.io.File
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.ScriptableObject
+import org.mozilla.javascript.annotations.JSGetter
+
+object ScriptableFile {
+  def apply(context: Context, file: File): ScriptableFile = {
+    val beyondContextFactory = context.getFactory.asInstanceOf[BeyondContextFactory]
+    val scope = beyondContextFactory.global
+    val args: JSArray = Array(file)
+    context.newObject(scope, "File", args).asInstanceOf[ScriptableFile]
+  }
+
+  def jsConstructor(context: Context, args: JSArray, constructor: JSFunction, inNewExpr: Boolean): ScriptableFile = {
+    val file = args(0) match {
+      case file: File =>
+        file
+      case path: String =>
+        new File(path)
+      case _ =>
+        throw new IllegalArgumentException("type.is.not.matched")
+    }
+    new ScriptableFile(file)
+  }
+}
+
+class ScriptableFile(file: File) extends ScriptableObject {
+  def this() = this(null)
+
+  override def getClassName: String = "File"
+
+  @JSGetter
+  def getName: String = file.getName
+
+  @JSGetter
+  def getPath: String = file.getPath
+
+  @JSGetter
+  def getIsFile: Boolean = file.isFile
+
+  @JSGetter
+  def getIsDirectory: Boolean = file.isDirectory
+}
