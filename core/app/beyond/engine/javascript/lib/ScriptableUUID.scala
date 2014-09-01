f93e8cbe355b74ab4f338f6695e78b5b03a6d3b2
Kwang Yul Seo <skyul@company100.com>
2014-09-01 18:40:44 +0900

    Add parse() to ScriptableUUID.
    parse creates a UUID from the string representation.

--- core/app/beyond/engine/javascript/lib/ScriptableUUID.scala
+++ core/app/beyond/engine/javascript/lib/ScriptableUUID.scala
@@ -1,7 +1,10 @@
 package beyond.engine.javascript.lib
 
+import beyond.engine.javascript.BeyondContext
+import beyond.engine.javascript.BeyondContextFactory
 import beyond.engine.javascript.JSArray
 import beyond.engine.javascript.JSFunction
+import com.beyondframework.rhino.ContextOps._
 import java.util.UUID
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
@@ -13,11 +16,23 @@ object ScriptableUUID {
   def v4(context: Context, thisObj: Scriptable, args: JSArray, function: JSFunction): String =
     UUID.randomUUID().toString
 
+  @JSStaticFunctionAnnotation
+  def parse(context: Context, thisObj: Scriptable, args: JSArray, function: JSFunction): ScriptableUUID = {
+    val beyondContextFactory = context.getFactory.asInstanceOf[BeyondContextFactory]
+    val scope = beyondContextFactory.global
+
+    val name = args(0).asInstanceOf[String]
+    val uuid = UUID.fromString(name)
+    context.newObject(scope, "UUID", uuid).asInstanceOf[ScriptableUUID]
+  }
+
   def jsConstructor(context: Context, args: JSArray, constructor: JSFunction, inNewExpr: Boolean): ScriptableUUID =
     new ScriptableUUID
 }
 
-class ScriptableUUID extends ScriptableObject {
+class ScriptableUUID(val uuid: UUID) extends ScriptableObject {
+  def this() = this(null)
+
   override def getClassName: String = "UUID"
 }
 
--- plugins/lib/uuid.js
+++ plugins/lib/uuid.js
@@ -2,3 +2,6 @@ exports.v4 = function () {
     return UUID.v4();
 };
 
+exports.parse = function (name) {
+    return UUID.parse(name);
+};
