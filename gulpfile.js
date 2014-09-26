04c804e908f69b138585964bcb526e6557fe7d61
Hyunje Alex Jun <noraesae@company100.com>
2014-09-26 19:02:23 +0100

    Add gulp-jshint task.
    The task is to lint JavaScript sources including plugin files and
    gulpfile itself.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
 
-gulp.task('default', function () {});
+gulp.task('jshint', function () {
+  return gulp.src([
+      './gulpfile.js',
+      './plugins/**/*.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter(jshintStylish));
+});
+
+gulp.task('default', ['jshint']);
