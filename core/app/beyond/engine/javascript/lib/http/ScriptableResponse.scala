d526f0062c86a61e3ddb1b5122a8b5b76015f0a6
Gilyoung Kim <roadzero@company100.com>
2014-11-05 12:22:07 +0900

    Add statusCode to response.
    Default status code is 200.

--- core/app/beyond/engine/javascript/lib/http/ScriptableResponse.scala
+++ core/app/beyond/engine/javascript/lib/http/ScriptableResponse.scala
@@ -6,7 +6,7 @@ import org.mozilla.javascript.Context
 import org.mozilla.javascript.ScriptRuntime
 import org.mozilla.javascript.ScriptableObject
 import play.api.mvc.Result
-import play.api.mvc.Results.Ok
+import play.api.mvc.Results.Status
 
 object ScriptableResponse {
   private def optionalArg[T](args: JSArray, idx: Int): Option[T] =
@@ -16,17 +16,21 @@ object ScriptableResponse {
       None
     }
 
+  private def optionalArgInt(args: JSArray, idx: Int): Option[Int] =
+    optionalArg[AnyRef](args, idx).map(ScriptRuntime.toInt32)
+
   private def optionalArgString(args: JSArray, idx: Int): Option[String] =
     optionalArg[String](args, idx).map(ScriptRuntime.toString)
 
   def jsConstructor(context: Context, args: JSArray, constructor: JSFunction, inNewExpr: Boolean): ScriptableResponse = {
     val result = args(0).asInstanceOf[String]
     val contentType = optionalArgString(args, 1).getOrElse("plain/text")
+    val statusCode = optionalArgInt(args, 2).getOrElse(play.api.http.Status.ACCEPTED)
 
     // FIXME: Support other HTTP status codes.
     // FIXME: Currently, the default type of contentType is plain/text,
     // but the play framework supports contentType inference. Fix it to use this feature.
-    new ScriptableResponse(Ok(result).as(contentType))
+    new ScriptableResponse(new Status(statusCode)(result).as(contentType))
   }
 }
 
