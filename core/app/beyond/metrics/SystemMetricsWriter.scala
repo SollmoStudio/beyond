3304f871f1e2a72c7a854a91d43616389c0452a9
Dong Ju Kang <celica212@company100.com>
2014-12-10 17:48:29 +0900

    Use same field names between mongodb collection and case class for metrics.

--- core/app/beyond/metrics/SystemMetricsWriter.scala
+++ core/app/beyond/metrics/SystemMetricsWriter.scala
@@ -27,8 +27,8 @@ object SystemMetricsWriter {
 
   sealed trait SystemMetrics
   case class SystemLoadAverage(hostname: String, date: Date, loadAverage: Double) extends SystemMetrics
-  case class HeapMemoryUsage(hostname: String, date: Date, usage: MemoryUsage) extends SystemMetrics
-  case class NonHeapMemoryUsage(hostname: String, date: Date, usage: MemoryUsage) extends SystemMetrics
+  case class HeapMemoryUsage(hostname: String, date: Date, memoryUsage: MemoryUsage) extends SystemMetrics
+  case class NonHeapMemoryUsage(hostname: String, date: Date, memoryUsage: MemoryUsage) extends SystemMetrics
   case class SwapMemoryUsage(hostname: String, date: Date, free: Long, total: Long, used: Long) extends SystemMetrics
   case class NumberOfRequestsPerSecond(hostname: String, date: Date, numberOfRequests: Int) extends SystemMetrics
 
@@ -70,7 +70,7 @@ object SystemMetricsWriter {
   implicit val numberOfRequestsPerSecondWrites: Writes[NumberOfRequestsPerSecond] = (
     (JsPath \ "hostname").write[String] and
     (JsPath \ "date").write[Date] and
-    (JsPath \ "count").write[Int]
+    (JsPath \ "numberOfRequests").write[Int]
   )(unlift(NumberOfRequestsPerSecond.unapply)).addTag("NumberOfRequestsPerSecond")
 }
 
