7499ddd275d52996fad00d86209f0cc5a4206d31
Seulgi Kim <dev@seulgi.kim>
2015-02-23 21:37:28 +0900

    Implement helper function to find metrics with tag name.

--- modules/admin/app/controllers/admin/Metrics.scala
+++ modules/admin/app/controllers/admin/Metrics.scala
@@ -2,8 +2,13 @@ package controllers.admin
 
 import java.lang.management.MemoryUsage
 import java.util.Date
+import play.api.libs.json.Format
+import play.api.libs.json.Json
 import play.api.mvc.Controller
 import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.api.QueryOpts
+import scala.concurrent.Future
 
 object Metrics extends Controller with MongoController {
   // FIXME: Share below code with beyondCore.
@@ -15,4 +20,17 @@ object Metrics extends Controller with MongoController {
   case class NonHeapMemoryUsage(hostname: String, date: Date, memoryUsage: MemoryUsage) extends SystemMetrics
   case class SwapMemoryUsage(hostname: String, date: Date, free: Long, total: Long, used: Long) extends SystemMetrics
   case class NumberOfRequestsPerSecond(hostname: String, date: Date, numberOfRequests: Int) extends SystemMetrics
+
+  private def metricsCollection: JSONCollection = db.collection[JSONCollection]("admin.metrics")
+
+  private def metrics[T](tag: String, maxNumber: Int)(implicit format: Format[T]): Future[Seq[T]] = {
+    import play.api.libs.concurrent.Execution.Implicits._
+
+    metricsCollection
+      .find(Json.obj("tag" -> tag))
+      .options(QueryOpts(batchSizeN = maxNumber))
+      .sort(Json.obj("date" -> -1))
+      .cursor[T]
+      .collect[Seq](upTo = maxNumber)
+  }
 }
